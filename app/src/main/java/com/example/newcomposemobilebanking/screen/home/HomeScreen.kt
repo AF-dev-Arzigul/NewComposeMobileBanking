4151935e52a49200d0acee6f881ae4d103557a63
AF-dev-Arzigul <arzigul.nazarbaeva1@s.amity.edu>
2023-01-16 17:01:11 +0500

    -auth part finished

--- app/src/main/java/com/example/newcomposemobilebanking/screen/home/HomeScreen.kt
+++ app/src/main/java/com/example/newcomposemobilebanking/screen/home/HomeScreen.kt
@@ -1,7 +1,14 @@
 package com.example.newcomposemobilebanking.screen.home
 
+
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.tooling.preview.Preview
 import cafe.adriel.voyager.androidx.AndroidScreen
+import cafe.adriel.voyager.hilt.getViewModel
+import com.example.newcomposemobilebanking.ui.theme.NewComposeMobileBankingTheme
+import com.example.newcomposemobilebanking.screen.home.HomeScreenContract.*
+
 
 /*
  * Arzigul Nazarbaeva
@@ -13,7 +20,31 @@ class HomeScreen : AndroidScreen() {
 
     @Composable
     override fun Content() {
-        TODO("Not yet implemented")
+        NewComposeMobileBankingTheme {
+            val viewModel: HomeViewModelImpl = getViewModel()
+            val uiState = viewModel.uiState.collectAsState().value
+            HomeScreenContent(uiState, viewModel::onEventDispatcher)
+        }
     }
+}
 
+@Composable
+fun HomeScreenContent(
+    uiState: UiState,
+    onEventDispatcher: (Intent) -> Unit
+) {
+
+}
+
+@Composable
+fun HomeActionBar() {
+
+}
+
+@Preview
+@Composable
+fun HomeScreenPreview() {
+    NewComposeMobileBankingTheme {
+        HomeScreenContent(uiState = UiState(""), onEventDispatcher = {})
+    }
 }
